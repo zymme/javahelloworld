ff2bd7c5ecd6a0a59dae8d29b8d34363bd5b9a34
Dave Zimmer <dave.zimmer@viawest.com>
2016-08-23 09:25:26 -0500

    added new feature

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -5,5 +5,7 @@ public class JavaHelloWorld {
   public static void main(String[] args) {
     System.out.println("Java Hello World!");
     System.out.println("Hello out there!!");
+
+    System.out.println("Adding in a new feature!!! SO COOL! (testing triggering auto build with docker hub!)");
   }
 }
